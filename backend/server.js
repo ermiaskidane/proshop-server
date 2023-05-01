3ac8376619891078af27c655c29eed0a18809632
ermias kidane <ermiaskidanegebre@gmail.com>
2023-05-01 09:51:03 +0100

    proshop server deploy

---
+++ backend/server.js
@@ -0,0 +1,92 @@
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import morgan from 'morgan'
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// help to access json format in the req.body
+app.use(express.json())
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+const __dirname = path.resolve() // c/z we use ES6 module we need to mimic
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.js'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
+
+// // Custom Error Handler for Routes does not exist
+app.use(notFound)
+
+// app.use((req, res, next) => {
+//     const error = new Error(`Not Found - ${req.originalUrl}`)
+//     res.status(404)
+//     next(error)
+// })
+
+// // Custom Error handler
+app.use(errorHandler)
+
+// app.use((err, req, res, next) => {
+//     const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+//     res.status(statusCode)
+//     res.json({
+//         message: err.message,
+//         stack: process.env.NODE_ENV === "production" ? null : err.stack,
+//     })
+// })
+
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold
+  )
+)
+
+// mongoose
+//   .connect(
+//     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-rl0mg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+//   )
+//   .then(() => {
+//     app.listen(process.env.PORT || 5000);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
