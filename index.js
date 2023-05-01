0b1a80e2910729409c6abe0e2380d700b44c3099
ermias kidane <ermiaskidanegebre@gmail.com>
2023-05-01 18:04:00 +0100

    install cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
+import cors from 'cors'
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -23,6 +24,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // help to access json format in the req.body
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
